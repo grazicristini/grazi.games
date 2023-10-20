b5ccc6a5298f013ad66a3282633cfc4888ab036c
grazicristini <grazielicristhyny2002@gmail.com>
2023-10-20 14:23:40 +0000

    criação de tabelas

---
+++ app/src/main/java/application/model/Genero.java
@@ -0,0 +1,42 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    @OneToMany(mappedBy = "genero")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+}
---
+++ app/src/main/java/application/model/Plataforma.java
@@ -0,0 +1,36 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plataformas")
+public class Plataforma {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    @ManyToMany(mappedBy = "plataformas")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
